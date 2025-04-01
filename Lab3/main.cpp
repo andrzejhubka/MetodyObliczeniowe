48417a9ff62cb692ccc4a8f490f75db1ba477bc1
Arnazell <andrzej_hubka@wp.pl>
2025-04-01 22:12:32 +0200

    4.0.0

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,110 +1,260 @@
 #include <iostream>
-#include <cfloat>
 #include <cmath>
 #include <iomanip>
 
 // kryteria zakonczenia iteracji
-constexpr long double TOL_X = 0.1;
-constexpr long double TOL_Y = 0.1;
+constexpr long double TOL_X = 1E-16;
+constexpr long double TOL_Y = 1E-16;
 constexpr long double MAX_N = 200;
 
-// funkcje i ich pochodne
-long double fun1(long double x);
-long double fun1_dfdx(long double x);
-
-long double fun2(long double x);
-long double fun2_dfdx(long double x);
-
+#define SHOW_F1 0
+#define SHOW_F2 1
+#define SHOW_PICARD 1
+#define SHOW_BISECTION 1
+#define SHOW_NEWTON 1
+#define SHOW_SECANT 1
 
 // realiazcja metod rozwiazywan pojedynczych rownan nieliniowych
-long double solver_picard(long double x);
-long double solver_bisection(long double x);
-long double solver_gauss(long double x);
-long double solver_secant(long double x);
+long double solver_picard(long double x_prev,
+                            long double (*defining_function)(long double),
+                            long double (*function)(long double),
+                            const std::string label);
+
+long double solver_bisection(long double a,
+                            long double b,
+                            long double(*function)(long double),
+                            std::string label);
+
+long double solver_newton(long double x,
+                            long double (*function)(long double),
+                            long double (*function_derivative)(long double),
+                            std::string label);
+
+long double solver_secant(long double x0,
+                            long double x1,
+                            long double x2,
+                            long double (*function)(long double),
+                            std::string label);
 
 int main()
 {
-    // picard
-    long double x_picard = 1.l;
-    for (int i=0; i < MAX_N; i++)
+    if (SHOW_PICARD)
     {
-        x_picard = solver_picard(x_picard);
+        if (SHOW_F1)
+        {
+            solver_picard(1.1,
+        [](long double x){ return 1.l - std::tanh(x)/2.l; },
+        [](long double x){return 2.l*x - 2.l + std::tanh(x);},
+        "Metoda picarda dla F1:");
+        }
+        if (SHOW_F2)
+        {
+            solver_picard(1.1,
+        [](long double x){ return std::asinh(1.l - x/4.l);},
+        [](long double x){return std::asinh(1.l - x/4.l)-x;},
+        "Metoda picarda dla F2:");
+        }
     }
-    std::cout <<std::setprecision(40)<< x_picard << std::endl;
 
-    // bisekcja
-    long double bisection_a = -100;
-    long double bisection_b = 100;
-    long double bisection_c = 0;
+    if (SHOW_BISECTION)
+    {
+        if(SHOW_F1)
+        {
+            solver_bisection(0, 100,
+        [](long double x){return std::tanh(x) + 2.l * (x - 1.l);},
+        "Metoda bisekcji dla F1:");
+        }
+        if(SHOW_F2)
+        {
+            solver_bisection(0, 100,
+       [](long double x){return std::asinh(1.l - x/4.l)-x;},
+       "Metoda bisekcji dla F2:");
+        }
+    }
+
+    if (SHOW_NEWTON)
+    {
+        if(SHOW_F1)
+        {
+            solver_newton(1.l,
+        [](long double x){return 2.l*x-2.l+std::tanh(x);},
+        [](long double x){return 2.l + 1/(std::cosh(x)*std::cosh(x));},
+        "Metoda newtona dla F1");
+        }
+        if(SHOW_F2)
+        {
+            solver_newton(1.l,
+        [](long double x){return std::asinh(1.l - x/4.l)-x;},
+        [](long double x){return -1.l/4.l * 1.l/std::sqrt(x*x+1)-1;},
+        "Metoda newtona dla F2");
+        }
+    }
+
+    if (SHOW_SECANT)
+    {
+        if(SHOW_F1)
+        {
+            solver_secant(100,
+               10,
+               5,
+               [](long double x){return 2.l*x - 2.l + std::tanh(x);},
+               "Metoda siecznych dla F1");
+        }
+        if(SHOW_F2)
+        {
+            solver_secant(100,
+                10,
+                5,
+                [](long double x){return std::asinh(1.l - x/4.l)-x;},
+                "Metoda siecznych dla F2");
+        }
+    }
+    return 0;
+}
+
+long double solver_picard(long double x_prev,
+                            long double (*defining_function)(long double),
+                            long double (*function)(long double),
+                            const std::string label)
+{
+    long double x_current = x_prev;
+    long double estymator;
+    long double reziduum;
     for (int i=0; i < MAX_N; i++)
     {
-        std::cout<<"BIsection A:" <<bisection_a << std::endl;
-        if ((fun1(bisection_a) >= 0 && fun1(bisection_b) < 0)
-            || (fun1(bisection_a) <= 0 && fun1(bisection_b) > 0))
+        x_prev = x_current;
+        x_current = defining_function(x_prev);
+
+        estymator = std::abs(x_current-x_prev);
+        reziduum = std::abs(function(x_current));
+
+        std::cout<<"--------"<<label<<"       iteracja: "<<i<<std::endl;
+        std::cout <<std::setprecision(70)<< "X:"<< x_prev << std::endl;
+        std::cout <<"Estymator bledu Xn: " <<estymator<<std::endl;
+        std::cout <<"Reziduum rownania: " << reziduum<<std::endl<<std::endl;;
+
+        if ((reziduum < TOL_Y) && (estymator < TOL_X))
+        {
+            std::cout << "Spelniono kryteria zakonczenia iteracji"<<std::endl<<std::endl;
+            break;
+        }
+    }
+
+    return x_current;
+}
+
+long double solver_bisection(long double a,
+                            long double b,
+                            long double (*function)(long double), std::string label)
+{
+    long double c = 0;
+    long double estymator;
+    long double reziduum;
+    long double x_current=0;
+
+    for (int i=0; i < MAX_N; i++)
+    {
+        if ((function(a) >= 0 && function(b) < 0)
+            || (function(a) <= 0 && function(b) > 0))
         {
             // gdy przedzial [a, b] zawiera rozwiazanie
-            bisection_c = bisection_b;
-            bisection_b = (bisection_a + bisection_c) / 2;
+            c = b;
+            b = (a + c) / 2;
         }
-        else if ((fun1(bisection_b) >= 0 && fun1(bisection_c) < 0)
-            || (fun1(bisection_b) <= 0 && fun1(bisection_c) > 0))
+        else if ((function(b) >= 0 && function(c) < 0)
+            || (function(b) <= 0 && function(c) > 0))
         {
             // gdy przedzial [b, c ] zawiera rozwiazanie
-            bisection_a = bisection_b;
-            bisection_b = (bisection_a + bisection_c) / 2;
+            a = b;
+            b = (a + c) / 2;
         }
         else
         {
             // brak rozwiazan
             std::cout << "Brak rozwiazan dla bisekcji"<<std::endl;
         }
-    }
-    std::cout << "bisekcja: "<< (bisection_c - bisection_a)/2 << std::endl;
+        x_current = (a+c)/2.l;
+        estymator = std::abs(b - a)/2;
+        reziduum = std::abs(function(x_current));
 
-    // metoda newtona
-    long double x_newton = 1.0l;
+
+        std::cout<<"--------"<<label<<"       iteracja: "<<i<<std::endl;
+        std::cout<<"X:" << x_current<< std::endl;
+        std::cout<<"Reziduum: " << reziduum<<std::endl;
+        std::cout<<"Estymator bledu xn: " <<estymator<<std::endl<<std::endl;
+
+        if ((estymator < TOL_X) && (reziduum < TOL_Y))
+        {
+            std::cout<<"Spelniono warunki zakoczenia iteracji"<<std::endl;
+            break;
+        }
+    }
+    return x_current;
+}
+
+long double solver_newton(long double x_prev,
+                            long double (*function)(long double),
+                            long double (*function_derivative)(long double),
+                            std::string label)
+                        {
+                            long double estymator;
+                            long double reziduum;
+                            long double x_current=x_prev;
+
+                            for (int i=0; i < MAX_N; i++)
+                            {
+                                x_prev = x_current;
+                                x_current -= function(x_prev) / function_derivative(x_prev);
+
+                                estymator = x_current - x_prev;
+                                reziduum = std::abs(function(x_current));
+
+
+                                std::cout<<"--------"<<label<<"       iteracja: "<<i<<std::endl;
+                                std::cout<<"X:" << x_current<< std::endl;
+                                std::cout<<"Reziduum: " << reziduum<<std::endl;
+                                std::cout<<"Estymator bledu xn: " <<estymator<<std::endl<<std::endl;
+
+                                if ((estymator < TOL_X) && (reziduum < TOL_Y))
+                                {
+                                    std::cout<<"Spelniono warunki zakoczenia iteracji"<<std::endl;
+                                    break;
+                                }
+                            }
+                            return x_current;
+                        }
+
+long double solver_secant(long double x0,
+                            long double x1,
+                            long double x2,
+                            long double (*function)(long double),
+                            std::string label)
+{
+    long double estymator;
+    long double reziduum;
+    long double x_prev=x2;
+
     for (int i=0; i < MAX_N; i++)
     {
-        std::cout<<"x_newton:"<<x_newton << std::endl;
-        x_newton = x_newton - (fun1(x_newton) / fun1_dfdx(x_newton));
+        x_prev=x2;
+        x2 = x1 - function(x1) / ((function(x1)-function(x0))/(x1-x0));
+        x0 = x1;
+        x1 = x2;
+
+        estymator = x2 - x_prev;
+        reziduum = std::abs(function(x2));
+
+
+        std::cout<<"--------"<<label<<"       iteracja: "<<i<<std::endl;
+        std::cout<<"X:" << x2<< std::endl;
+        std::cout<<"Reziduum: " << reziduum<<std::endl;
+        std::cout<<"Estymator bledu xn: " <<estymator<<std::endl<<std::endl;;
+
+        if ((estymator < TOL_X) && (reziduum < TOL_Y))
+        {
+            std::cout<<"Spelniono warunki zakoczenia iteracji"<<std::endl;
+            break;
+        }
     }
-    std::cout << "Newton: "<< x_newton<< std::endl;
-
-    // metoda siecznych
-    long double x_2_sieczne = 1;
-    long double x_1_sieczne = 2;
-    long double x_0_sieczne = 3;
-
-    for (int i=0; i < MAX_N; i++)
-    {
-        x_2_sieczne = x_1_sieczne - fun1(x_1_sieczne) / ((fun1(x_1_sieczne)-fun1(x_0_sieczne))/(x_1_sieczne-x_0_sieczne));
-        x_0_sieczne = x_1_sieczne;
-        x_1_sieczne = x_2_sieczne;
-        std::cout << "Sieczne: "<< x_2_sieczne << std::endl;
-    }
-    std::cout << "Sieczne: "<< x_2_sieczne << std::endl;
-    return 0;
-}
-
-long double solver_picard(long double x)
-{
-    return 1 - 0.5l * std::tanh(x);
-}
-
-
-long double fun1(long double x)
-{
-    return std::tanh(x) + 2 * (x - 1);
-}
-long double fun1_dfdx(long double x)
-{
-    return (1 / (std::cosh(x) * std::cosh(x))) + 2;
-}
-long double fun2(long double x)
-{
-    ;
-}
-long double fun2_dfdx(long double x)
-{
-    ;
+    return x2;
 }
--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,3 +1,186 @@
-//
-// Created by andrzej on 3/15/25.
-//
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <math.h>
+#include <chrono>
+#include <sstream>
+#include <cstdlib>
+
+constexpr int MAX_ITERATIONS = 30;
+constexpr long double TOL_X = 1e-100;
+constexpr long double TOL_Y = 1e-100;
+
+// obliczanie wartosci wektora funkcij F dla zadanych x,y,z
+void calculate_F(const long double (&X)[3], long double (&F)[3]);
+
+// obliczanie wartosci macierzy jacobiego
+void calculate_J(const long double (&X)[3], long double (&J)[3][3]);
+
+// obliczanie wyznacznika macierzy 3x3
+long double calculate_determinant(long double (&D)[3][3]);
+
+// rozwiazywanie ukladu rownan, ktorego wynikkiem jest wektor który nalezy odjac od obecnego rozwiazania, zeby przyblizyc sie do miejsca zerowego funkcji.
+void solve_linear(long double (&D)[3], long double (&F)[3], long double (&J)[3][3]);
+
+inline long double f1(long double x, long double y, long double z);
+inline long double f2(long double x, long double y, long double z);
+inline long double f3(long double x, long double y, long double z);
+
+int main()
+{
+  // wektor przyblizonych wynikow X
+  long double X[3] = {1.l, 2.l, 3.l};
+
+  // wektor poprzednich wyników -> do obliczania estymatora bledu
+  long double X_prev[3] = {1.l, 2.l, 3.l};
+
+  // wektor wartosci funkcji F(X)
+  long double F[3];
+
+  // macierz jacobiego J
+  long double J[3][3];
+
+  // wektor poprawy wyniku D
+  long double D[3];
+
+  // glowna iteracja
+  for (int i=0; i<MAX_ITERATIONS; i++)
+  {
+    // obliczenie wartosci funkcji w wektorze F i macierzy jacobiego
+    calculate_F(X, F);
+    calculate_J(X, J);
+
+    // obliczenie wektora poprawek
+    solve_linear(D, F, J);
+
+    X_prev[0] = X[0];
+    X_prev[1] = X[1];
+    X_prev[2] = X[2];
+
+    // zastosowanie poprawek
+    X[0] -= D[0];
+    X[1] -= D[1];
+    X[2] -= D[2];
+
+    std::cout<<"Iteracja; "<< i+1<<std::endl;
+    std::cout << "X: " <<std::setprecision(70) << std::fixed << X[0]<<std::endl;
+    std::cout <<  "Y: "<<std::setprecision(70) << std::fixed << X[1]<<std::endl;
+    std::cout <<  "Z: "<<std::setprecision(70) << std::fixed << X[2]<<std::endl;
+    std::cout << "suma residuum dla rownan 1,2,3: " << f1(X[0], X[1], X[2]) + f2(X[0], X[1], X[2]) + f3(X[0], X[1], X[2]) << std::endl;
+    std::cout << "suma estymatorow bledow Xn: " << abs(X_prev[0] - X[0]) + abs(X_prev[1] - X[1]) + abs(X_prev[2] - X[2]);
+    std::cout << std::endl << std::endl;
+
+    // jesli spelniono kryteria wiarygodnosci przyblizenia pierwiastka i dokladnosci wyznaczenia
+    if ((abs(f1(X[0], X[1], X[2])) <= TOL_Y) &&
+        (abs(f2(X[0], X[1], X[2])) <= TOL_Y)&&
+         (abs(f2(X[0], X[1], X[2]) <= TOL_Y)) &&
+           (abs(X_prev[0] - X[0]) <= TOL_X) &&
+             (abs(X_prev[1] -X[1] <= TOL_X)) &&
+               (abs(X_prev[2] -X[2] <= TOL_X)))
+
+    {
+      std::cout << "Spelniono kryteria dokladnosci wyznaczenia Xn i wiarygodnosci Xn jako pierwiastka" <<std::endl;
+      break;
+    }
+  }
+
+
+
+
+  return 0;
+}
+
+void calculate_J(const long double (&X)[3], long double (&J)[3][3])
+{
+  J[0][0] = 2 * X[0];
+  J[0][1] = 2 * X[1];
+  J[0][2] = 2 * X[2];
+
+  J[1][0] = 2 * X[0];
+  J[1][1] = X[1];
+  J[1][2] = 0;
+
+  J[2][0] = X[1];
+  J[2][1] = X[0];
+  J[2][2] = 0;
+}
+
+void calculate_F(const long double (&X)[3], long double (&F)[3])
+{
+  F[0] = X[0]*X[0] + X[1]*X[1] + X[2]*X[2] -4l;
+  F[1] = X[0]*X[0] + X[1]*X[1]/2l - 1l;
+  F[2] = X[0]*X[1] - 0.5l;
+}
+
+void solve_linear(long double (&D)[3], long double (&F)[3], long double (&J)[3][3])
+{
+  // sprawdz czy macierz nie jest osobliwa
+  if (abs(calculate_determinant(J)) < 1e-16)
+  {
+    std::cout<<"Rownanie jest osobliwe a układ równań nie jest LN";
+    exit(0);
+  }
+
+  // Eliminacja Gaussa z częściowym wyborem elementu głównego
+  int order[3] = {0,1,2};
+
+  for (int i = 0; i < 2; i++)
+  {
+    // Znajdź wiersz z maksymalnym elementem w kolumnie i
+    int maxRow = i;
+    long double maxVal = std::abs(J[order[i]][i]);
+    for (int k = i + 1; k < 3; k++)
+    {
+      if (std::abs(J[order[k]][i]) > maxVal)
+      {
+        maxVal = std::abs(J[order[k]][i]);
+        maxRow = k;
+      }
+    }
+
+    // Zamień wiersze jeśli potrzeba
+    if (maxRow != i)
+    {
+      std::swap(order[i], order[maxRow]);
+    }
+
+    // Eliminacja
+    for (int k = i + 1; k < 3; k++)
+    {
+      long double factor = J[order[k]][i] / J[order[i]][i];
+      F[order[k]] -= factor * F[order[i]];
+      J[order[k]][i] = 0;
+      for (int j = i+1; j < 3; j++)
+      {
+        J[order[k]][j] -= factor * J[order[i]][j];
+      }
+    }
+  }
+
+  D[2] = F[order[2]] / J[order[2]][2];
+  D[1] = (F[order[1]] - J[order[1]][2] * D[2]) / J[order[1]][1];
+  D[0] = (F[order[0]] - J[order[0]][1] * D[1] - J[order[0]][2] * D[2]) / J[order[0]][0];
+}
+
+long double calculate_determinant(long double (&X)[3][3])
+{
+  // regula sarrusa
+  const long double result = X[0][0] * X[1][1] * X[2][2] + X[0][1] * X[1][2] * X[2][0] + X[0][2]*X[1][0]*X[2][1]
+          - X[0][2] * X[1][1] * X[2][0] - X[0][0] * X[1][2] * X[2][1] + X[0][1]*X[1][0]*X[2][2];
+
+  return result;
+}
+
+inline long double f1(long double x, long double y, long double z)
+{
+  return x*x + y*y + z*z - 4.l;
+}
+inline long double f2(long double x, long double y, long double z)
+{
+  return x*x + y*y/2.l -1.l;
+}
+inline long double f3(long double x, long double y, long double z)
+{
+  return x*y -0.5l;
+}
